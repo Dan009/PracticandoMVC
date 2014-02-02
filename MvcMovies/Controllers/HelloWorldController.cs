947bdf8f868bf73981a79a9ba62dcb8c0d91b96f
Dan009 <dan_009marte@hotmail>
2014-02-02 11:33:47 -0400

    Primer commit
    primet commit hecho en github

---
+++ MvcMovies/Controllers/HelloWorldController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovies.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+
+        public ActionResult Index() {
+            return View();
+            
+        
+        }
+
+        //
+        // GET: /HelloWorld/Welcome
+
+        public ActionResult Welcome(string name = "", int numTimes = 1)
+        {
+            name = (name == "") ? "D: i dont know you :c !!!" : name;
+            ViewBag.Message = "Hello "+name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+
+        }
+
+       
+
+    }
+}
---
+++ MvcMovies/Controllers/MoviesController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMovies.Models;
+using System.Linq.Expressions;
+
+namespace MvcMovies.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly MovieDBContext _db = new MovieDBContext();
+
+        //
+        // GET: /Movies/
+
+        public ActionResult Index()
+        {
+            return View(_db.Movies.ToList());
+        }
+
+        public ActionResult SearchIndex(string id) {
+
+            string searchString = id;
+            var movies = from movieResult in _db.Movies
+                         select movieResult;
+
+            if(!String.IsNullOrEmpty(searchString)){
+                movies.Where(a => a.Title.Contains(searchString));
+            
+            }
+
+            return View(movies);
+        }
+
+        //
+        // GET: /Movies/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Movie movie = _db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        //
+        // GET: /Movies/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Movies/Create
+
+        [HttpPost]
+        public ActionResult Create(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Movies.Add(movie);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(movie);
+        }
+
+        //
+        // GET: /Movies/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Movie movie = _db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        //
+        // POST: /Movies/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(movie).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(movie);
+        }
+
+        //
+        // GET: /Movies/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Movie movie = _db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        //
+        // POST: /Movies/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Movie movie = _db.Movies.Find(id);
+            _db.Movies.Remove(movie);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
